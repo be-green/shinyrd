bd0bfe7be0df7830ff5a77ead93266dc158fe2cb
be-green <brice.c.green@gmail.com>
2020-02-26 22:30:17 -0600

    first draft of covariate balance plots

--- R/covariate-balance.R
+++ R/covariate-balance.R
@@ -7,53 +7,79 @@
 #' @importFrom data.table `:=`
 #' @importFrom ggplot2 ggplot
 #' @param data Data to use for covariate balance checks
-#' @param running_variable running variable to use for checks
+#' @param rv The running variable to use in the discontinuity
 #' @param cov Vector of covariate names to plot
-#' @param method Method to use for balance checks
-#' @param ... other parameters to pass to function
-#' @details The method argument defaults to a linear regression
+#' @param geom Representation binned points, the name of any geom.
+#' See [the ggplot2 website](https://ggplot2.tidyverse.org/reference/)
+#' @param fit_method Method to use to fit regression line for balance checks
+#' @param aggregate Whether to aggregate observations within each bin for visualization purposes
+#' @param ... other parameters to be passed to geom_smooth, the call which plots the line
+#' @details
+#' Covariate balance tests allow us to visualize
+#' whether important characteristics differ on either side
+#' of the regression discontinuity boundary.
+#'
+#' The sum_method indicates how data is meant to be binned
+#' for visualization purposes. Currently it defaults to "raw"
+#' but it will also accept "mean", "median", and "pointrange",
+#' which will plot a point-range for each bin on the x axis.
+#'
+#' The fit_method argument defaults to a linear regression
 #' using all the data. Other acceptable arguments are "loess"
 #' for local linear regression, "gam" for generalized additive
 #' models, "quadratic" for a quadratic fit using the whole
 #' dataset.
 #' @export
-
 plot_covbal <- function(data,
-                        method = c("lm", "loess", "gam", "quad"),
-                        running_variable,
+                        rv,
                         cov,
+                        aggregate = T,
+                        geom = "point",
+                        fit_method = "lm",
                         binwidth = NULL,
                         cutoff,
                         ...) {
 
   if(is.null(binwidth)) {
-    binwidth <- guess_binwidth(data[[running_variable]])
+    binwidth <- guess_binwidth(data[[rv]])
   }
 
   data <- as.data.table(data)
-  data[,Group := findInterval(get(running_variable), cutoff)]
+  data[,Group := findInterval(get(rv), cutoff)]
 
   plot_data <- data[,.SD, .SDcols = c(
     "Group",
-    running_variable,
+    rv,
     cov
   )]
 
   plot_data[,Interval := findInterval(
-    get(running_variable),
+    get(rv),
     seq(0, max(Group + 1), by = binwidth)
   )]
 
-  bin_lvls <- seq(min(data[[running_variable]]),
-                  max(data[[running_variable]]),
+  bin_lvls <- seq(min(data[[rv]]),
+                  max(data[[rv]]),
                   by = binwidth)
+
+  if(fit_method == "quad") {
+    smooth_method <- "lm"
+  } else {
+    smooth_method <- fit_method
+  }
+
+  if(fit_method == "quad") {
+    smooth_form = as.formula(y ~ x + I(x^2))
+  } else {
+    smooth_form = as.formula(y ~ x)
+  }
 
   plot_data <- merge(plot_data,
                      data.table(Interval = 1:length(bin_lvls),
                                 Level = bin_lvls),
                            by = "Interval")
 
-  melt(plot_data, id.vars = c("Group", "Interval", running_variable, "Level"),
+  plot <- melt(plot_data, id.vars = c("Group", "Interval", rv, "Level"),
                     variable.name = "Variable",
                     value.name = "Value",
                     ) %>%
@@ -61,12 +87,20 @@ plot_covbal <- function(data,
                     ggplot2::aes_string(x = "Level",
                                         y = "Value",
                                         group = "Group")) +
-    ggplot2::geom_smooth(method = "lm") +
-    ggplot2::stat_summary(fun.y = mean, geom = "point") +
+    ggplot2::geom_smooth(method = smooth_method, formula = smooth_form) +
     facet_wrap(~Variable, scales = 'free_y') +
     xlab("Level of Running Variable") +
     ylab("Value of Covariate") +
     geom_vline(xintercept = cutoff)
+
+  if(aggregate) {
+    plot + ggplot2::stat_summary(fun.y = mean,
+                                 fun.ymax = function(x) mean(x) + 2*sd(x)/sqrt(length(x)),
+                                 fun.ymin = function(x) mean(x) - 2*sd(x)/sqrt(length(x)),
+                                 geom = geom)
+  } else {
+    plot + getFromNamespace(paste0("geom_", geom), ns = "ggplot2")()
+  }
 
 }
 
